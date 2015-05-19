5a6af4a9095f22ddd3259556d67fbe61878be4eb
webmandesign <webmandesigneu@gmail.com>
2015-05-19 09:09:17 +0200

    2.2.1
    Updating docblock

--- class-post-formats.php
+++ class-post-formats.php
@@ -31,7 +31,7 @@ if ( ! class_exists( '{%= prefix_class %}_Post_Formats' ) ) {
 	 * @link  https://github.com/webmandesign/wp-post-formats
 	 * @link  http://www.webmandesign.eu
 	 *
-	 * @version  2.2
+	 * @version  2.2.1
 	 *
 	 *
 	 * GENERATED MEDIA
